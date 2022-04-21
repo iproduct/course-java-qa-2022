48cadecb0791a9c7cf89ecd0daa6e9e04eadd4e7
Trayan Iliev <office@iproduct.org>
2022-04-21 20:38:27 +0300

    Person class

--- java-qa-01-intro/src/course/qa/hello/HelloJava.java
+++ java-qa-01-intro/src/course/qa/hello/HelloJava.java
@@ -1,16 +1,24 @@
 package course.qa.hello;
 
-import java.util.Date;
 import course.qa.formatter.HelloFormatter;
+import course.qa.model.Person;
+
+import java.util.Arrays;
+import java.util.Date;
 import java.util.Scanner;
 
 public class HelloJava {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter your name:");
-        String name = sc.nextLine();
-        HelloFormatter hf = new HelloFormatter();
-        System.out.println(hf.format(name));
-        System.out.println(new Date());
+//        System.out.println("Commnd line args: " + Arrays.toString(args));
+//        Scanner sc= new Scanner(System.in);
+//        System.out.println("Enter your name:");
+//        String name = sc.nextLine();
+//        HelloFormatter hf = new HelloFormatter();
+//        System.out.println(hf.format(name));
+//        System.out.println(new Date());
+
+        Person p1 = new Person("John Doe");
+        p1.setName("John Smith Doe");
+        System.out.println(p1.getName());
     }
 }
