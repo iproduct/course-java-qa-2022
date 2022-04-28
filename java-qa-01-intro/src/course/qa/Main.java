961424dfc48eec865aa337243d5b854ab2bea8d7
Trayan Iliev <office@iproduct.org>
2022-04-28 19:18:39 +0300

    project updated

---
+++ java-qa-01-intro/src/course/qa/Main.java
@@ -0,0 +1,15 @@
+package course.qa;
+
+import course.qa.model.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person[] persons = {
+                new Person(1L, "John", "Doe", 29),
+                new Person(2L, "Jane", "Doe", 25),
+                new Person(3L, "Jim", "Doe", 36),
+        };
+
+//        System.out.println(p1);
+    }
+}
--- java-qa-01-intro/src/course/qa/model/Person.java
+++ java-qa-01-intro/src/course/qa/model/Person.java
@@ -1,17 +1,81 @@
 package course.qa.model;
 
+import java.util.StringJoiner;
+
 public class Person {
-    private String name = null;
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private int age;
 
-    public Person(String name) {
-        this.name = name;
+    public Person() {
     }
 
-    public String getName() {
-        return name;
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public Person(Long id, String firstName, String lastName, int age) {
+        this(firstName, lastName, age);
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+
+        Person person = (Person) o;
+
+        return getId() != null ? this.getId().equals(person.getId()) : person.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Person.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("firstName='" + firstName + "'")
+                .add("lastName='" + lastName + "'")
+                .add("age=" + age)
+                .toString();
     }
 }
