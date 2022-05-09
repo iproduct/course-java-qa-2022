339bd54c8f3fe474004774466b7d35c911e04ad7
Trayan Iliev <office@iproduct.org>
2022-05-09 20:39:32 +0300

    WordCountDemo complete

--- java-qa-01-intro/src/course/qa/wordcount/WordCountDemo.java
+++ java-qa-01-intro/src/course/qa/wordcount/WordCountDemo.java
@@ -1,9 +1,6 @@
 package course.qa.wordcount;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class WordCountDemo {
 
@@ -22,7 +19,7 @@ public class WordCountDemo {
             "hadn't", "herself", "nor", "should've", "am", "once", "ve", "an", "hers", "myself", "to", "her", "is",
             "shan't", "are", "same", "being", "your", "than", "it", "aren", "shan", "d", "m", "isn't", "down",
             "shouldn't", "you're", "why", "o", "yourself", "has", "my", "ma", "out", "that", "mustn't", "because",
-            "after", "before", "most", "off", "mustn", "won't");
+            "after", "before", "most", "off", "mustn", "won't", "must");
     public static final String WIKI_TEXT = """
             Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible. It is a general-purpose programming language intended to let programmers write once, run anywhere (WORA),[17] meaning that compiled Java code can run on all platforms that support Java without the need to recompile.[18] Java applications are typically compiled to bytecode that can run on any Java virtual machine (JVM) regardless of the underlying computer architecture. The syntax of Java is similar to C and C++, but has fewer low-level facilities than either of them. The Java runtime provides dynamic capabilities (such as reflection and runtime code modification) that are typically not available in traditional compiled languages. As of 2019, Java was one of the most popular programming languages in use according to GitHub,[19][20] particularly for client–server web applications, with a reported 9 million developers.[21]
                 
@@ -80,19 +77,38 @@ public class WordCountDemo {
             }
             var words = line.split("\\W+");
             for(String word: words){
-                if(word.length() < 3){
+                word = word.toLowerCase();
+                if(word.length() < 3 || STOP_WORDS.contains(word)){
                     continue;
                 }
-                var wordLower = word.toLowerCase();
-                Integer count = wordCounts.get(wordLower);
+                if(word.endsWith("s")) {
+                    word = word.substring(0, word.length() - 1);
+                }
+
+                Integer count = wordCounts.get(word);
                 if(count != null) {
-                    wordCounts.put(wordLower, count + 1);
+                    wordCounts.put(word, count + 1);
                 } else {
-                    wordCounts.put(wordLower, 1);
+                    wordCounts.put(word, 1);
                 }
 //                wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
             }
         }
-        System.out.println(wordCounts);
+        var wcList = new ArrayList<>(wordCounts.entrySet());
+//        wcList.sort(new CountComparator()); // 1)
+//        wcList.sort(new Comparator<Map.Entry<String, Integer>>() { // 2)
+//            @Override
+//            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+//                return o2.getValue().compareTo(o1.getValue());
+//            }
+//        });
+        wcList.sort( (o1,o2) -> o2.getValue().compareTo(o1.getValue()) ); // 3)
+        var iter = wcList.iterator();
+        int count = 15; // top 15 keywords
+        while(iter.hasNext() && count-- > 0){
+            var elem = iter.next();
+            System.out.printf("%-25s -> %d%n", elem.getKey(), elem.getValue());
+        }
+
     }
 }
