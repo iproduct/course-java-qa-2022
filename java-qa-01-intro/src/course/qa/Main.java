b664e4b750fa4adc84fd5dfda257202e4c46eda8
Trayan Iliev <office@iproduct.org>
2022-05-19 19:27:43 +0300

    CRUD demo + presentation Stream API added

--- java-qa-01-intro/src/course/qa/Main.java
+++ java-qa-01-intro/src/course/qa/Main.java
@@ -10,36 +10,16 @@ import course.qa.model.User;
 
 import java.util.List;
 
+import static course.qa.model.Role.AUTHOR;
+
 public class Main {
     public static void main(String[] args) {
-//        Person[] persons = {
-//                new Person(1L, "John", "Doe", 29),
-//                new Person(2L, "Jane", "Doe", 25),
-//                new Person(3L, "Jim", "Doe", 36),
-//        };
-
-//        for(Person p : persons) {
-//            System.out.println(p);
-//        }
-//        for(int i = 0; i < persons.length; i++) {
-//            System.out.println(persons[i]);
-//        }
-//        int i = 0;
-//        while(i < persons.length) {
-//            System.out.println(persons[i]);
-//            i++;
-//        }
-//        do {
-//            System.out.println(persons[i]);
-//            i++;
-//        } while (i < persons.length);
-
         // Users demo
         List<User> users = List.of(
                 new User("Ivan", "Petrov", 28, "ivan", "ivan123"),
                 new User("John", "Doe", 42, "john", "john123", Role.ADMIN,
                         "+(1) 23424242323", true),
-                new User("Jane", "Doe", 34, "jane", "jane123", Role.AUTHOR,
+                new User("Jane", "Doe", 34, "jane", "jane123", AUTHOR,
                         "+(359) 889654532", true)
         );
 
@@ -54,6 +34,21 @@ public class Main {
 //        }
 //        userRepo.findAll().forEach(user -> System.out.println(user));
         userRepo.findAll().forEach(System.out::println);
-        System.out.println(userRepo.findByUsername("john"));
+        var john = userRepo.findByUsername("john").get();
+        System.out.println("Before update: " + john);
+
+        //update user data
+        john.setRole(AUTHOR);
+        john.setPhone("+359 8888888888");
+        john.setPassword("aaaaaaaa123#");
+        john.setAge(45);
+        userRepo.update(john);
+        var johnAfterUpdate = userRepo.findById(john.getId());
+        System.out.println("After update: " + johnAfterUpdate);
+
+        // delete user
+        userRepo.deleteById(john.getId());
+        System.out.println("After delete:");
+        userRepo.findAll().forEach(System.out::println);
     }
 }
