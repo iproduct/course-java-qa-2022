9b929d5620811d00d8f357539de6b17f8e4c6f56
Trayan Iliev <office@iproduct.org>
2022-05-30 19:06:02 +0300

    @ExceptionHandler methods added

--- 03-spring-rest/src/main/java/course/qa/spring/web/UsersController.java
+++ 03-spring-rest/src/main/java/course/qa/spring/web/UsersController.java
@@ -1,5 +1,8 @@
 package course.qa.spring.web;
 
+import course.qa.spring.exception.InvalidEntityDataException;
+import course.qa.spring.exception.NonexistingEntityException;
+import course.qa.spring.model.ErrorResponse;
 import course.qa.spring.model.User;
 import course.qa.spring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
 @RequestMapping("/api/users")
@@ -33,6 +39,18 @@ public class UsersController {
                         .pathSegment("{id}")
                         .buildAndExpand(created.getId()).toUri())
                 .body(created);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleInvalidEntityDataException(InvalidEntityDataException ex){
+        return ResponseEntity.badRequest().body(
+                new ErrorResponse(BAD_REQUEST.value(), ex.getMessage(), ex.getViolations()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleNonexistingEntityException(NonexistingEntityException ex){
+        return ResponseEntity.status(NOT_FOUND).body(
+                new ErrorResponse(NOT_FOUND.value(), ex.getMessage()));
     }
 
 }
