db2fae3c66e0fc66b211c2f368a5eea88cc41f60
Trayan Iliev <office@iproduct.org>
2022-06-02 20:14:28 +0300

    StringUtils debugged and jupiter params dep added

--- 04-junit5/src/main/java/course/qa/simple/StringUtils.java
+++ 04-junit5/src/main/java/course/qa/simple/StringUtils.java
@@ -2,7 +2,7 @@ package course.qa.simple;
 
 public class StringUtils {
     public static boolean isPalindrome(String str) {
-        for (int i = 0, j = str.length(); i < j; i++, j--) {
+        for (int i = 0, j = str.length() - 1; i < j; i++, j--) {
             if (str.charAt(i) != str.charAt(j)) {
                 return false;
             }
