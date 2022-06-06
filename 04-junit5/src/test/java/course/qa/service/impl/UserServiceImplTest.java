90f0a30a36d7d12d36cc6f596f0e797bbb4b60f3
Trayan Iliev <office@iproduct.org>
2022-06-06 19:37:32 +0300

    dynamic junit tests demo added

---
+++ 04-junit5/src/test/java/course/qa/service/impl/UserServiceImplTest.java
@@ -0,0 +1,36 @@
+package course.qa.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void addUser() {
+    }
+
+    @Test
+    void addUsersBatch() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUserById() {
+    }
+
+    @Test
+    void count() {
+    }
+}
