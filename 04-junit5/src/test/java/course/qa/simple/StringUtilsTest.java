d6648a7d5fe8df855c72727ec88f8e3baa5b802d
Trayan Iliev <office@iproduct.org>
2022-06-06 17:39:42 +0300

    StringUtils debugged and jupiter params dep added

--- 04-junit5/src/test/java/course/qa/simple/StringUtilsTest.java
+++ 04-junit5/src/test/java/course/qa/simple/StringUtilsTest.java
@@ -5,26 +5,51 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class StringUtilsTest {
-    private static StringUtils stringUtils;
 
-    @BeforeAll
-    public static void setUp() {
-        stringUtils = new StringUtils();
+    @ParameterizedTest(name = "#{index}: Given {0} isPalindrome -> true")
+    @ValueSource(strings = {"racecar", "radar", "able was I ere I saw elba"})
+    void given_radar_whenIsPalindrome_thenTrue(String candidate) {
+        assertTrue(StringUtils.isPalindrome(candidate));
     }
 
-    @Test
-    @Description("Given 'radar' isPalindrome should return true")
-    void given_radar_whenIsPalindrome_thenTrue() {
-
+    @ParameterizedTest(name = "#{index}: Given {0} isPalindrome -> {1}")
+    @MethodSource("stringAndBooleanResultProvider")
+    void givenMethodSource_IsPalindrome(String candidate, boolean expectedResult) {
+        assertEquals(expectedResult, StringUtils.isPalindrome(candidate));
     }
 
-    @Test
-    @Description("Given 'byl hlyb' isPalindrome should return false")
-    void given_byl_hlyb_whenIsPalindrome_thenFalse() {
+    @ParameterizedTest(name = "#{index}: Given {0} isPalindrome -> {1}")
+    @CsvSource({
+            "racecar, true",
+            "radar, true",
+            "able was I ere I saw elba, true",
+            "byl hlyb, false"
+    })
+    void givenCsvSource_IsPalindrome(String candidate, String expectedResult) {
+        assertEquals(Boolean.valueOf(expectedResult), StringUtils.isPalindrome(candidate));
+    }
 
+    @ParameterizedTest(name = "#{index}: Given {0} isPalindrome -> {1}")
+    @CsvFileSource(resources = "/palindroms.csv", numLinesToSkip = 1)
+    void givenCsvFileSource_IsPalindrome(String candidate, String expectedResult) {
+        assertEquals(Boolean.valueOf(expectedResult), StringUtils.isPalindrome(candidate));
+    }
+
+    static Stream<Arguments> stringAndBooleanResultProvider() {
+        return Stream.of(
+                arguments("racecar", true),
+                arguments("radar", true),
+                arguments("able was I ere I saw elba", true),
+                arguments("byl hlyb", false)
+        );
     }
 }
