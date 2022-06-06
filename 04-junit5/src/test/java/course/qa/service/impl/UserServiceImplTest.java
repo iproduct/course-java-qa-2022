f69b61451498feeb95bbfebe3752f73d9e0fe672
Trayan Iliev <office@iproduct.org>
2022-06-06 20:17:34 +0300

    Jackson data read from JSON file

--- 04-junit5/src/test/java/course/qa/service/impl/UserServiceImplTest.java
+++ 04-junit5/src/test/java/course/qa/service/impl/UserServiceImplTest.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
@@ -23,11 +24,13 @@ class UserServiceImplTest {
     @BeforeAll
     public static void setup() {
         ObjectMapper mapper = new ObjectMapper();
+        mapper.findAndRegisterModules();
         URL usersJsonFileUrl = UserServiceImplTest.class.getClassLoader().getResource("users.json");
         try {
             TypeReference<List<User>> userList = new TypeReference<List<User>>() { };
             SAMPLE_USERS = mapper.readValue(usersJsonFileUrl, userList);
-            log.info("User loaded from JSON file: {}", SAMPLE_USERS);
+            log.info("User loaded from JSON file: {}",
+                    SAMPLE_USERS.stream().map(User::toString).collect(Collectors.joining("\n")));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
