69b61041a9856690c9251b53bcc768684c9b6103
Trayan Iliev <office@iproduct.org>
2022-06-06 20:09:23 +0300

    pom deps for Jackson and Mockito

--- 04-junit5/src/test/java/course/qa/service/impl/UserServiceImplTest.java
+++ 04-junit5/src/test/java/course/qa/service/impl/UserServiceImplTest.java
@@ -1,20 +1,36 @@
 package course.qa.service.impl;
 
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import course.qa.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.Collection;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
+@Slf4j
 class UserServiceImplTest {
     private static List<User> SAMPLE_USERS;
 
     @BeforeAll
     public static void setup() {
-
+        ObjectMapper mapper = new ObjectMapper();
+        URL usersJsonFileUrl = UserServiceImplTest.class.getClassLoader().getResource("users.json");
+        try {
+            TypeReference<List<User>> userList = new TypeReference<List<User>>() { };
+            SAMPLE_USERS = mapper.readValue(usersJsonFileUrl, userList);
+            log.info("User loaded from JSON file: {}", SAMPLE_USERS);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 //        SAMPLE_USERS
     }
 
