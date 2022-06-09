27bdda109faf265937fc22f46f689689e5251874
Trayan Iliev <office@iproduct.org>
2022-06-09 21:30:56 +0300

    Selenium tests project update

--- 06-selenium-next/src/test/java/selenium/WebdriverManagerTest.java
+++ 06-selenium-next/src/test/java/selenium/WebdriverManagerTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
