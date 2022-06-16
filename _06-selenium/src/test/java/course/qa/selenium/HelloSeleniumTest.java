3fbb90761b8d0bf62ced7ef5bdc8164e44cf48a3
Trayan Iliev <office@iproduct.org>
2022-06-16 18:29:07 +0300

    hello Selenium test complete

--- _06-selenium/src/test/java/course/qa/selenium/HelloSeleniumTest.java
+++ _06-selenium/src/test/java/course/qa/selenium/HelloSeleniumTest.java
@@ -1,10 +1,14 @@
 package course.qa.selenium;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
 public class HelloSeleniumTest {
     @Test
     public void openChromePage() {
@@ -16,6 +20,19 @@ public class HelloSeleniumTest {
 
         // Exercise
         driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        String title = driver.getTitle();
 
+        // Assert
+        assertThat(title).contains("Selenium WebDriver");
+
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            log.warn("Test interrupted", e);
+//        }finally {
+
+        //cleanup
+        driver.quit();
+//        }
     }
 }
