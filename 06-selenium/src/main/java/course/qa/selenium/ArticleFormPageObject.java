09e1f0c5ae6c0876df4c81dc8a578f7a7ca4339f
Trayan Iliev <office@iproduct.org>
2022-06-17 11:31:48 +0300

    Page Object demo - @CacheLookup annotation aded

--- 06-selenium/src/main/java/course/qa/selenium/ArticleFormPageObject.java
+++ 06-selenium/src/main/java/course/qa/selenium/ArticleFormPageObject.java
@@ -2,6 +2,7 @@ package course.qa.selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
@@ -19,5 +20,6 @@ public class ArticleFormPageObject {
     public WebElement title;
 
     @FindBy(how = How.NAME, using = "content")
+    @CacheLookup // for better test performance
     public WebElement content;
 }
